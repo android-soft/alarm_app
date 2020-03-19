741e337d78080a7b7459d56f91eb7b864d7dfdbd
Diseño Web Cantabria | Desarrollo Web Cantabria <62180604+Diseno-Desarrollo-Web-App-Cantabria@users.noreply.github.com>
2020-03-19 10:45:10 +0100

    Create en_r.js

---
+++ en_r.js
@@ -0,0 +1,21 @@
+{"name":   [
+    "BBC Radio 2", 
+    "BBC Radio 4",
+    "BBC Radio 1",
+    "Classic FM",
+    "BBC Radio 5",
+    "Smooth Grooving FM",
+    "Kiss FM"
+    "Magic Radio"
+     ], 
+"uri" : [
+        "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_q",
+        "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4fm_mf_q",
+        "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_q",
+        "http://classicfmlagos.atunwadigital.streamguys1.com/classicfmlagos",
+        "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio5live_mf_q",
+        "http://64.95.243.43:8020/;",
+        "http://live.radiokiss.gr/kissfm1006hq.mp3",
+        "http://magicradio.ice.infomaniak.ch/magicradio-high.mp3"
+     ]         
+                                          }
