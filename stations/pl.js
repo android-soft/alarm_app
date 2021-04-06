ebd2a11554e5645278e744e3d8c11a8d2a66a47e
android-soft <80132106+android-soft@users.noreply.github.com>
2021-04-07 00:51:15 +0200

    Update pl.js

--- stations/pl.js
+++ stations/pl.js
@@ -4,7 +4,7 @@
   "Radio ZET",
   "RMF FM",
   "TOK FM",
-  "Radio Esca"
+  "Trojka"
      ], 
 
  "uri" : [
@@ -13,6 +13,6 @@
    "https://n-6-12.dcs.redcdn.pl/sc/o2/Eurozet/live/audio.livx",
    "https://rs6-krk2.rmfstream.pl/RMFFM48",
    "https://radiostream.pl/tuba10-1.mp3",
-   "https://radio.stream.smcdn.pl/icradio-p/2380-1.aac/playlist.m3u8"
+   "https://stream13.polskieradio.pl/pr3/pr3.sdp/media_w1958169685_1256134.aac"
      ]         
 }
