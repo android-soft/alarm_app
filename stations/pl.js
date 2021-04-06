6ffd2d1ca788b599bc080e2728d794a1ad295a82
android-soft <80132106+android-soft@users.noreply.github.com>
2021-04-07 00:59:36 +0200

    Update pl.js

--- stations/pl.js
+++ stations/pl.js
@@ -13,6 +13,6 @@
    "https://n-6-12.dcs.redcdn.pl/sc/o2/Eurozet/live/audio.livx",
    "https://rs6-krk2.rmfstream.pl/RMFFM48",
    "https://radiostream.pl/tuba10-1.mp3",
-   "http://stream13.polskieradio.pl/pr3/pr3.sdp/media_w980361479_1256154.aac"
+   "http://mp3.polskieradio.pl:8904/;.mp3"
      ]         
 }
