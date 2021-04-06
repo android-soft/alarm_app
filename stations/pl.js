09caff21fef4ce844ff97f67d691f7420a045845
android-soft <80132106+android-soft@users.noreply.github.com>
2021-04-07 00:46:47 +0200

    Update pl.js

--- stations/pl.js
+++ stations/pl.js
@@ -4,7 +4,7 @@
   "Radio ZET",
   "RMF FM",
   "TOK FM",
-  "West FM"
+  "Radio Esca"
      ], 
 
  "uri" : [
@@ -13,6 +13,6 @@
    "https://n-6-12.dcs.redcdn.pl/sc/o2/Eurozet/live/audio.livx",
    "https://rs6-krk2.rmfstream.pl/RMFFM48",
    "https://radiostream.pl/tuba10-1.mp3",
-   "https://stream-mz.planetradio.co.uk/westfm.aac"
+   "https://radio.stream.smcdn.pl/icradio-p/2380-1.aac/playlist.m3u8"
      ]         
 }
