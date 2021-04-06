aa08147716a05413b51879096143ee5c4d1d67d4
android-soft <80132106+android-soft@users.noreply.github.com>
2021-04-07 00:38:46 +0200

    Update pl.js

--- stations/pl.js
+++ stations/pl.js
@@ -1,10 +1,18 @@
 {"name":   [
   "Radio Plus",
-  "Top Wszech Czasów"
+  "Top Wszech Czasów",
+  "Radio ZET",
+  "RMF FM",
+  "TOK FM",
+  "West FM"
      ], 
 
  "uri" : [
    "https://plu.cdn.eurozet.pl/plu-waw.mp3",
-   "https://stream.open.fm/152"
+   "https://stream.open.fm/152",
+   "https://n-6-12.dcs.redcdn.pl/sc/o2/Eurozet/live/audio.livx",
+   "https://rs6-krk2.rmfstream.pl/RMFFM48",
+   "https://radiostream.pl/tuba10-1.mp3",
+   "https://stream-mz.planetradio.co.uk/westfm.aac"
      ]         
 }
