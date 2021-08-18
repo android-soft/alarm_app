8cc7b249d3e5c72a62e3a23b7550568a2e0b321f
Alexei Suzdalenko <80132106+android-soft@users.noreply.github.com>
2021-08-18 17:03:18 +0200

    Update channel.js

--- blog_rus/channel.js
+++ blog_rus/channel.js
@@ -1,7 +1,7 @@
 [
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCv5dc2Zi6j5IXQ_NKTZuSLA',
   'https://www.youtube.com/feeds/videos.xml?channel_id=UCNYsieni0V71jCgwkka79yQ',
-  'https://www.youtube.com/channel/UCHbNxnZIGbsrb-1vpExUqPw',
-  'https://www.youtube.com/channel/UCQkBKu3VZ6d96KJz_ixCq4g',
-  'https://www.youtube.com/watch?v=r66xIG1VV0E',
-  'https://www.youtube.com/channel/UCChdLsV2IM6WsYDWiivCJdw'
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCHbNxnZIGbsrb-1vpExUqPw',
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCQkBKu3VZ6d96KJz_ixCq4g',
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCChdLsV2IM6WsYDWiivCJdw' 
 ]
