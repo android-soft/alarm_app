fee7f492e86d2ea7b3c370052b72fa042f853c1c
Alexei Suzdalenko <80132106+android-soft@users.noreply.github.com>
2021-12-14 17:58:44 +0100

    Update france.js

--- stations/france.js
+++ stations/france.js
@@ -1,8 +1,12 @@
 {"name":   [
-    "RTL"
+    "RTL",
+    "Europe 1",
+    "RFI"
      ], 
 
  "uri" : [
-        "https://icecast.rtl.fr/rtl-1-44-128"
+        "https://icecast.rtl.fr/rtl-1-44-128",
+        "https://stream.europe1.fr/europe1.mp3",
+        "https://rfimonde64k.ice.infomaniak.ch/rfimonde-64.mp3"
      ]         
 }
