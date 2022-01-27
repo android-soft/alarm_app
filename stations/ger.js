6979364a2af301878eb12f0306650d3f40dcdd50
Alexei Suzdalenko <80132106+android-soft@users.noreply.github.com>
2022-01-27 10:36:29 +0100

    Update ger.js

--- stations/ger.js
+++ stations/ger.js
@@ -1,12 +1,20 @@
 {"name":   [
   "WRD",
   "WDR 1 Live",
-  "Harmony.fm"
+  "Harmony.fm",
+  "Antenne Bayern",
+  "Antenne Bayern Top 40",
+  "Antenne Bayern Chillout",
+  "Antenne Bayern DanceXXL"
      ], 
 
  "uri" : [
    "https://wdr-wdr4-live.sslcast.addradio.de/wdr/wdr4/live/mp3/128/stream.mp3",
    "https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3",
-   "https://mp3.harmonyfm.de/harmonyfm/hqlivestream.mp3"
+   "https://mp3.harmonyfm.de/harmonyfm/hqlivestream.mp3",
+   "https://s2-webradio.antenne.de/antenne",
+   "https://s8-webradio.antenne.de/top-40",
+   "https://s5-webradio.antenne.de/chillout",
+   "https://s8-webradio.antenne.de/dance-xxl/stream/mp3"
      ]         
 }
